f6e62ab06ba121097ddc4a066c48b797b6df4733
Sam Wolk <mail@samwolk.info>
2020-02-24 12:57:15 -0500

    remove application icon

--- knotApplication.py
+++ knotApplication.py
@@ -8,7 +8,7 @@ print("--------------------------------------")
 print('loading GUI...')
 
 root = tk.Tk()
-root.tk.call('wm', 'iconphoto', root._w, tk.PhotoImage(file='trefoil.png'))
+# root.tk.call('wm', 'iconphoto', root._w, tk.PhotoImage(file='trefoil.png'))
 gui = gui.KnotGUI(root,800,800)
 root.state('zoomed')
 root.mainloop()
